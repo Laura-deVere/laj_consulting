083406d40a4ff4d9669dcc666477cbcea35e1295
James Alexander <james@jmslxndr.com>
2015-09-03 12:14:53 -0400

    b-sinatra.rb updated

--- business-sinatra/b-sinatra.rb
+++ business-sinatra/b-sinatra.rb
@@ -5,3 +5,23 @@ get '/' do
 	@title = "LAJ Consulting - Home"
 	erb :home
 end
+
+get '/ourteam' do 
+	@title = "LAJ Consulting - Our Team"
+	erb :about
+end
+
+get '/contact' do 
+	@title = "LAJ Consulting - Contact"
+	erb :contact
+end
+
+get '/portfolio' do 
+	@title = "LAJ Consulting - Portfolio"
+	erb :portfolio
+end
+
+get '/blog' do 
+	@title = "LAJ Consulting - Blog"
+	erb :about
+end
