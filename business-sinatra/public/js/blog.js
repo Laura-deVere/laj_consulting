449ed1c033f311ef139809ee65b3e7aeb875afb0
Tyrek McRae <tyrekmcrae@gmail.com>
2015-09-04 17:15:29 -0400

    Ty's updates javascript

---
+++ business-sinatra/public/js/blog.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$( "#puppies" ).click(function() {
+  $( "#puppies" ).animate({
+    width: "70%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    height: "3em"
+  }, 1500 );
+});
+});
